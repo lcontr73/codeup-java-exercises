1ebcdabadace760268ab2d157bb6d739d5b2e583
Leonard Contreras <leonard.contreras03@gmail.com>
2022-06-07 12:30:44 -0500

    adjusting syntax types exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -60,18 +60,18 @@ public class HelloWorld {
 //        int x = 4;
 //        x += 5;
 //        System.out.println(x);
-
+//
 //        int x = 3;
 //        int y = 4;
 //        y *= x;
 //        System.out.println(y);
-
-        int x = 10;
-        int y = 2;
-        x /= y;
-        y -= x;
-        System.out.println(x);
-        System.out.println(y);
+//
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
 
     }
 }
