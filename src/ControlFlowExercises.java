c59a44bce15b21e2659f2bec52215034f5709193
Leonard Contreras <leonard.contreras03@gmail.com>
2022-06-07 15:01:39 -0500

    square on each loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,22 +1,36 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
         //Create an integer variable i with a value of 5.
-        int i = 5;
+//        int i = 5;
         //Create a while loop that runs so long as i is less than or equal to 15
-        while (i <= 15) {
-            System.out.println(i);
-            i++;
-        }
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
 
 
+        //Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+//        int x = 0;
+//        do {
+//            System.out.println(x);
+//            x = x + 2;
+//        } while (x >= -10);
 
-        //int x = 0;
-        int x = 100;
+
+        //Alter your loop to count backwards by 5's from 100 to -10.
+//        int y = 100;
+//        do {
+//            System.out.println(y);
+//            y = y - 5;
+//        } while (y >= -10);
+
+        //Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+
+        long z = 2;
         do {
-            System.out.println(x);
-            //x = x + 2;
-            x = x - 5;
-        } while (x >= -10);
+            System.out.println(z);
+            z = z*z;
+        } while (z < 100000);
 
     }
 }
